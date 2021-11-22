0b1694fd482be65ecccbf7c9baaa18340416e2da
xtream2000c <carlmarcast@gmail.com>
2021-11-22 19:55:43 +0100

    Iniciado Repositorio

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu principal</title>
     <meta author = "Carlos Maroto Castaño">
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <header>
