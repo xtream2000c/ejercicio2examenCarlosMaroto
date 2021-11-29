5de938059da42412cd4f9998f670858394a6e7c7
xtream2000c <carlmarcast@gmail.com>
2021-11-29 20:30:53 +0100

    Comentado el EJ2

--- Ejercicio2ExamenCarlosMaroto/iniciaSesion.php
+++ Ejercicio2ExamenCarlosMaroto/iniciaSesion.php
@@ -10,25 +10,25 @@
 <body>
 <?php
 
-    session_start();
-    $usuario = $_POST["usuario"];
+    session_start(); //Se inicia la sesion
+    $usuario = $_POST["usuario"]; //Se recibe el usuario y la contraseña
     $contrasena = $_POST["contrasena"];
 
-    if($usuario == $contrasena){
-        if(!isset( $_SESSION[strval($usuario)])){
-        $_SESSION[strval($usuario)] = 1;
+    if($usuario == $contrasena){ //Si son iguales entonces
+        if(!isset( $_SESSION[strval("u".$usuario)])){ //Si no existe la sesion con valor usuario
+        $_SESSION[strval("u".$usuario)] = 1; //Se crea la sesion con valor usuario y se da la bienvenida por primera vez
         echo "Bienvenido ". $usuario;
-        }else{
-            $veces = $_SESSION[strval($usuario)];
+        }else{ // Si ya existe la sesion con valor usuario, entonces
+            $veces = $_SESSION[strval("u".$usuario)]; //Se cuenta cuantas veces ha entrado en la aplicacionesta sesion
 
-            $_SESSION[strval($usuario)] = $veces + 1;
+            $_SESSION[strval("u".$usuario)] = $veces + 1; //Se le da un nuevo valor a la sesion
 
-            echo "Hola, bienvenido de nuevo a nuestra aplicacion ". $usuario;
+            echo "Hola, bienvenido de nuevo a nuestra aplicacion ". $usuario; //Se da la bienvenida de nuevo
             
         }
         
     }else{
-        header("Location: fallaSesion.php");
+        header("Location: fallaSesion.php"); //Si no coinciden contraseña y usuario, da fallo, y te manda a otra pagina donde se informa de esto.s
     }
 
 ?>
